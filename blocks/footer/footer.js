47d271b0f2e0337d69a552b835c2ddf60ccde975
Katie Sexton <ksexton@adobe.com>
2022-06-29 23:19:11 -0600

    feat: footer block

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -4,16 +4,22 @@ import { readBlockConfig, decorateIcons } from '../../scripts/scripts.js';
  * loads and decorates the footer
  * @param {Element} block The header block element
  */
-
 export default async function decorate(block) {
-  const cfg = readBlockConfig(block);
+  const config = readBlockConfig(block);
   block.textContent = '';
 
-  const footerPath = cfg.footer || '/footer';
+  const footerPath = config.footer || '/footer';
   const resp = await fetch(`${footerPath}.plain.html`);
   const html = await resp.text();
   const footer = document.createElement('div');
   footer.innerHTML = html;
   await decorateIcons(footer);
+
+  const classes = ['copyright', 'contact'];
+  classes.forEach((e, j) => {
+    const section = footer.children[j];
+    if (section) section.classList.add(`footer-${e}`);
+  });
+
   block.append(footer);
 }
